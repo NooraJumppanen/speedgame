b6746ed20b333e03c50c0e62f7139bf81df888e4
NooraJumppanen <noora.jumppanen@gmail.com>
2021-11-15 12:46:12 +0200

    added circles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import './App.css';
+import Circle from './Circle';
+import {circles} from './circles.js';
+
 
 class App extends Component {
+  state = {}
   render() {
     return (
+      <main>
+        <h1>Speedgame</h1>
+        <h2>Your score: <span className="scorearea"></span></h2>
+
+        <div className="circlewrapper">
+        {circles.map((c) => (<Circle key={c.id} color={c.color} id={c.id}/>))}
+       </div>
       <div>
-        
+        <button>start</button>
+        <button>stop</button>
       </div>
+      </main>
+
     );
   }
 }
